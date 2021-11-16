01a3b1a966426a2b5a3d297d5164dcaa819d2abe
bartekm777 <31894234+bartekm777@users.noreply.github.com>
2021-11-17 00:03:37 +0100

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
-import { PostDetailsComponent } from './post-details/post-details.component';
 
 const routes: Routes = [
   {
@@ -12,15 +11,6 @@ const routes: Routes = [
   {
     path: 'feed',
     component: FeedComponent,
-  },
-  {
-    path: 'post/:post',
-    component: PostDetailsComponent,
-  },
-  {
-    path: '**',
-    pathMatch: 'full',
-    redirectTo: 'feed',
   },
 ];
 
